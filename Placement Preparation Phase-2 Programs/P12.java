cbe3805ca0bd377dac80dfc04ccb85f793cfa9d2
Gohel Krish <137276566+Gohelkrish173@users.noreply.github.com>
2025-08-11 13:35:20 +0530

    Add New Programs. Octal To Decimal, And Pattern P12

---
+++ Placement Preparation Phase-2 Programs/P12.java
@@ -0,0 +1,41 @@
+// * * * * * * *
+//   *       *
+//     *   *
+//       *
+//     *   *
+//   *       *
+// * * * * * * *
+
+public class P12{
+    public static void main(String[] args){
+        // for(int i=1;i<=10;i++){
+        pattern(7);
+    }
+
+    public static void pattern(int n){
+        int mid = n+1/2;
+
+        for(int i = 0;i<n;i++){
+            if(i <= mid){
+                
+                for(int k = 0 ; k < n; k++){
+                    if(i == 0 || i == n-1 || i == k || k == n-i-1){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }
+            }else{
+                for(int j = 0; j < n ; j++){
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+            
+            
+
+        }
+
+    }
+}
---
+++ Placement Preparation Phase-2 Programs/P9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+// Decimal to HexaDecimal
+
+public class P9{
+    public static void mainI(String[] args){
+
+    }
+
+    public static String decimalToHexa(double x){
+
+    }
+    
+}
